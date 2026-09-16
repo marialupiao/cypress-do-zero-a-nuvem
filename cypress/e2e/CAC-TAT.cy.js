639ecb634ba597fb3f72c13dcad8e006c1ebeba8
Maria Carolina Lupião <mcgl89@gmail.com>
2026-09-16 10:12:02 -0300

    licao10

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -34,7 +34,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('[name="lastName"]').type('da Silva')
     cy.get(':nth-child(2) > :nth-child(1) > [name="email"]').type('mariadasilva@teste.teste')
     cy.get('[name="open-text-area"]').type('Agradeço pela atenção')
-    cy.contains('Enviar').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
 
@@ -57,7 +57,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get(':nth-child(2) > :nth-child(1) > [name="email"]').type('joaodesouza@teste.com')
     cy.get('#check > [name="phone"]').check()
     cy.get('[name="open-text-area"]').type('Agradeço pela atenção')
-    cy.contains('Enviar').click()
+    cy.contains('button', 'Enviar').click()
 
     cy.get('.error').should('be.visible')
 
@@ -95,7 +95,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   // exercício extra 6
   it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
 
-    cy.contains('Enviar').click()
+    cy.contains('button', 'Enviar').click()
     cy.get('.error').should('be.visible')
 
   })
@@ -115,4 +115,105 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('.success').should('be.visible')
 
   })
+
+  it('seleciona um produto (YouTube) por seu texto', () => {
+
+    cy.get('#product')
+    .select('YouTube')
+    .should('have.value', 'youtube') 
+  
+  })
+
+  it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+
+    cy.get('#product')
+    .select('mentoria')
+    .should('have.value', 'mentoria')
+
+  })
+
+  it('seleciona um produto (Blog) por seu índice', () => {
+
+    cy.get('#product')
+    .select(1)
+    .should('have.value', 'blog')
+
+  })
+  
+  it('marca o tipo de atendimento "Feedback"', () => {
+
+    cy.get('input[type="radio"][value="feedback"]')
+    .check()
+    .should('be.checked') 
+
+  })
+
+  it('marca cada tipo de atendimento', () => {
+
+    cy.get('input[type="radio"]')
+    .should('have.length', 3)
+    .each(($radio) => {
+      cy.wrap($radio).check()
+      cy.wrap($radio).should('be.checked')
+    })
+
+  })
+
+  it('marca ambos checkboxes, depois desmarca o último', () => {
+
+    cy.get('input[type="checkbox"]')
+    .check()
+    .should('be.checked')
+    .last()
+    .uncheck()
+    .should('not.be.checked') 
+
+  })
+
+  it('seleciona um arquivo da pasta fixtures', () => {
+
+    cy.get('input[type="file"]#file-upload')
+    .should('not.have.value')
+    .selectFile('./cypress/fixtures/example.json')
+    .should(($input) => {
+      expect($input[0].files[0].name).to.equal('example.json')
+    })
+
+  })
+
+    it('seleciona um arquivo simulando um drag-and-drop', () => {
+
+      cy.get('input[type="file"]#file-upload')
+      .should('not.have.value')
+      .selectFile('./cypress/fixtures/example.json', { action: 'drag-drop' })
+      .should(($input) => {
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+
+    })
+
+    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+
+      cy.fixture('example.json').as('sampleFile') 
+    
+    })
+  
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+
+      cy.contains('a', 'Política de Privacidade')
+      .should('have.attr', 'href', 'privacy.html')
+      .and('have.attr', 'target', '_blank')
+
+    })
+
+    it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+
+      cy.contains('a', 'Política de Privacidade')
+      .invoke('removeAttr', 'target')
+      .click()
+
+      cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
+    })
+
+    
 })
